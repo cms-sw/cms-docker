6904372ca3062a482b41d8566aa2364a7f8748aa
Giulio Eulisse <giulio.eulisse@cern.ch>
2014-11-14 11:19:43 +0100

    Cleanup personal references.

--- cmsdist-provider-tester/manifest.pp
+++ cmsdist-provider-tester/manifest.pp
@@ -7,11 +7,14 @@ package {["HEP_OSlibs_SL6", "e2fsprogs"]:
 file {"/etc/sudoers.d/999-cmsbuild-requiretty":
    content => "Defaults:root !requiretty\n",
 }->
+user {"cmsbuild":
+  ensure => present,
+}->
 file {"/opt/cms":
   ensure => directory,
   owner => "cmsbuild",
 }->
-package {"cms+cmssw+CMSSW_7_1_0":
+package {"lcg+SCRAMV1+V2_2_6_pre2":
   ensure             => present,
   provider           => cmsdist,
   install_options    => [{
