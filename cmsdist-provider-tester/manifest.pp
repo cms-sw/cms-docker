ba508e65efde738edd15d51d399ea973e983d1c2
Giulio Eulisse <giulio.eulisse@cern.ch>
2014-11-14 10:52:08 +0100

    Sample container to test the cmsdist provider.

---
+++ cmsdist-provider-tester/manifest.pp
@@ -0,0 +1,24 @@
+# An example puppet file which installs CMSSW into 
+# /opt/cms.
+
+package {["HEP_OSlibs_SL6", "e2fsprogs"]:
+  ensure => present,
+}->
+file {"/etc/sudoers.d/999-cmsbuild-requiretty":
+   content => "Defaults:root !requiretty\n",
+}->
+file {"/opt/cms":
+  ensure => directory,
+  owner => "cmsbuild",
+}->
+package {"cms+cmssw+CMSSW_7_1_0":
+  ensure             => present,
+  provider           => cmsdist,
+  install_options    => [{
+    "install_prefix" => "/opt/cms",
+    "install_user"   => "cmsbuild",
+    "architecture"   => "slc6_amd64_gcc481",        
+    "server" => "cmsrep.cern.ch",        
+    "server_path" => "cmssw/cms",
+  }]
+}
