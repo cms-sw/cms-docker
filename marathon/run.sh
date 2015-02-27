b2f55e341096e5bee1918f1ce072e914f32b52a8
Giulio Eulisse <giulio.eulisse@cern.ch>
2015-02-27 16:00:33 +0100

    Make marathon customizable.

--- marathon/run.sh
+++ marathon/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e -x
-cat << EOF > /etc/mesos/zk
-zk://${MARATHON_ZK-zk}:2181/mesos
-EOF
-
-marathon
+java -Djava.library.path=/usr/local/lib:/usr/lib:/usr/lib64 \
+     -Djava.util.logging.SimpleFormatter.format=%2$s%5$s%6$s%n \
+     -Xmx512m -cp /usr/bin/marathon mesosphere.marathon.Main \
+     --zk zk://${MARATHON_ZK-zk}:2181/marathon \
+     --master zk://${MARATHON_ZK-zk}:2181/mesos
