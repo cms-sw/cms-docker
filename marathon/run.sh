96657ff2ae7e3d51cad192209750483de266fc14
Giulio Eulisse <giulio.eulisse@cern.ch>
2015-03-29 11:00:49 +0200

    Get marathon to work in host mode by default.

--- marathon/run.sh
+++ marathon/run.sh
@@ -2,8 +2,8 @@
 java -Djava.library.path=/usr/local/lib:/usr/lib:/usr/lib64 \
      -Djava.util.logging.SimpleFormatter.format=%2$s%5$s%6$s%n \
      -Xmx512m -cp /usr/bin/marathon mesosphere.marathon.Main \
-     --zk ${MARATHON_ZK-zk://zk:2181/marathon} \
-     --master ${MESOS_MASTER-zk://zk:2181/mesos} \
+     --zk ${MARATHON_ZK-zk://localhost:2181/marathon} \
+     --master ${MESOS_MASTER-zk://localhost:2181/mesos} \
      ${HTTP_PORT+--http_port $HTTP_PORT} \
      --hostname `hostname` \
      ${MARATHON_HA+--ha} \
