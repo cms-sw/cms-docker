a31f90c212404d79d5c515c18f5c6132f9aa139e
Giulio Eulisse <giulio.eulisse@cern.ch>
2015-04-01 14:53:18 +0200

    Moved to kibana 1.4.1

--- kibana/run.sh
+++ kibana/run.sh
@@ -1,37 +1,30 @@
 #!/bin/sh -ex
 
 # Use configuration from commandline.
-
 KIBANA_ES=${KIBANA_ES-localhost:9200}
-KIBANA_DEFAULT_ROUTE=${KIBANA_DEFAULT_ROUTE-/dashboard/file/default.json}
 KIBANA_INDEX=${KIBANA_INDEX-kibana-int}
 
-cat <<EOF >/usr/share/nginx/html/kibana/config.js
-define(['settings'],
-function (Settings) {
-  return new Settings({
-    elasticsearch: window.location.protocol + "//${KIBANA_ES}",
-    default_route     : '${KIBANA_DEFAULT_ROUTE}',
-    kibana_index: "${KIBANA_INDEX}",
-    panel_names: [
-      'histogram',
-      'map',
-      'goal',
-      'table',
-      'filtering',
-      'timepicker',
-      'text',
-      'hits',
-      'column',
-      'trends',
-      'bettermap',
-      'query',
-      'terms',
-      'stats',
-      'sparklines'
-    ]
-  });
-});
+cat <<EOF >/kibana/config/kibana.yml
+port: 5601
+host: "0.0.0.0"
+elasticsearch_url: ${KIBANA_ES-http://localhost:9200}
+elasticsearch_preserve_host: true
+kibana_index: ".kibana"
+default_app_id: "${KIBANA_DEFAULT_APP-discover}"
+request_timeout: 300000
+shard_timeout: 0
+bundled_plugin_ids:
+ - plugins/dashboard/index
+ - plugins/discover/index
+ - plugins/doc/index
+ - plugins/kibana/index
+ - plugins/markdown_vis/index
+ - plugins/metric_vis/index
+ - plugins/settings/index
+ - plugins/table_vis/index
+ - plugins/vis_types/index
+ - plugins/visualize/index
 EOF
 # Start the crontab and run nginx
-nginx 
+cd /kibana
+bin/kibana
