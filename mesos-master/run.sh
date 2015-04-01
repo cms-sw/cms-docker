f3c51d3524616f0714755f136304547cbcf85bb0
Giulio Eulisse <giulio.eulisse@cern.ch>
2015-04-01 12:29:43 +0200

    Use localhost by default for ZK in mesos-master.

--- mesos-master/run.sh
+++ mesos-master/run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -e -x
-mesos-master --zk=${MESOS_MASTER_ZK-zk://zk:2181/mesos} \
+mesos-master --zk=${MESOS_MASTER_ZK-zk://localhost:2181/mesos} \
              --quorum=${QUORUM-1} \
              --work_dir=${MESOS_MASTER_WORKDIR-/var/lib/mesos} \
              --log_dir=${MESOS_LOG_DIR-/var/log/mesos} \
