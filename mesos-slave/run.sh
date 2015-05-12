459fe975e197c24d922c1e04e87e48055a1454e1
Giulio Eulisse <giulio.eulisse@cern.ch>
2015-05-12 13:35:27 +0200

    Improve defaults.
    Use localhost:2181 rather than zk:2181 as default for ZooKeeper.

--- mesos-slave/run.sh
+++ mesos-slave/run.sh
@@ -4,7 +4,7 @@
 #
 # Notice that in case of a frontend, we expose port 53 as well
 # as a valid resource.
-mesos-slave --master=${MESOS_MASTER_ZK-zk://zk:2181/mesos} \
+mesos-slave --master=${MESOS_MASTER_ZK-zk://localhost:2181/mesos} \
              --work_dir=${MESOS_MASTER_WORKDIR-/var/lib/mesos} \
              ${MESOS_SLAVE_FRONTEND+--resources='ports(*):[31000-32000, 53-53]'} \
              --containerizers=docker,mesos \
