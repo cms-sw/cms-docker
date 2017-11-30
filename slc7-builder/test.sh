25b2a42208d9c7d3bd96c05a4d2fb7e8d29965ed
Malik Shahzad Muzaffar <shahzad.malik.muzaffar@cern.ch>
2017-11-30 14:14:10 +0100

    Update test.sh

--- slc7-builder/test.sh
+++ slc7-builder/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ex
 whoami
-cd /tmp
+cd $WORKSPACE
 wget http://cmsrep.cern.ch/cmssw/bootstrap.sh
 sh ./bootstrap.sh -a slc7_amd64_gcc630 setup
 sh ./bootstrap.sh -a slc7_amd64_gcc700 setup
