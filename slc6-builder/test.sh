53eeac0cf339d57ac6f922b66f723bd0f4120149
Malik Shahzad Muzaffar <shahzad.malik.muzaffar@cern.ch>
2017-11-30 14:52:44 +0100

    Update test.sh

--- slc6-builder/test.sh
+++ slc6-builder/test.sh
@@ -2,7 +2,7 @@
 whoami
 cd $WORKSPACE
 wget http://cmsrep.cern.ch/cmssw/bootstrap.sh
-for gcc in gcc472 gcc481 gcc491 gcc493 gcc530 gcc600 gcc630 gcc700 ; do
+for gcc in gcc472 gcc481 gcc491 gcc493 gcc530 gcc630 gcc700 ; do
   sh ./bootstrap.sh -a slc6_amd64_$gcc setup
 done
 echo ALL_OK
