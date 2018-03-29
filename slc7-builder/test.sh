a62bb2dd6db6e6c92535eea972fc10c605532549
Malik Shahzad Muzaffar <shahzad.malik.muzaffar@cern.ch>
2018-03-29 18:33:06 +0200

    Update test.sh

--- slc7-builder/test.sh
+++ slc7-builder/test.sh
@@ -1,8 +1,14 @@
 #!/bin/bash -ex
+ARCH="amd64"
+GCC_VER="gcc493 gcc530 gcc630 gcc700"
+if [ $(uname -m) = "aarch64" ] ; then 
+  ARCH="aarch64"
+  GCC_VER="gcc700"
+fi
 whoami
 cd $WORKSPACE
 wget http://cmsrep.cern.ch/cmssw/bootstrap.sh
-for gcc in gcc493 gcc530 gcc630 gcc700 ; do
-  sh ./bootstrap.sh -a slc7_amd64_$gcc setup
+for gcc in ${GCC_VER} ; do
+  sh ./bootstrap.sh -a slc7_${ARCH}_${gcc} setup
 done
 echo ALL_OK
