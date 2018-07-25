240e04d72d53b6621a8cc2dac27d8e6f5b6cd866
Zygimantas Matonis <zygimantas.matonis@cern.ch>
2018-07-25 18:15:59 +0200

    added EXECUTE_BUILD for slc7-builder builder

---
+++ slc7-builder/EXECUTE_BUILD_ARM64.sh
@@ -0,0 +1,9 @@
+# Do not foget to change tag on each update or prevous version will be overwriten
+# CONTAINER_TAG=latest
+# DOCKERHUB_USER="Use job default"
+# EXTRA_BUILD_ARGS=
+PUSH_CONTAINER=true
+DOCKER_FILE=Dockerfile.arm64  
+# CONTAINER_NAME="Get from commit dir" 
+# GITHUT_REPO="Use job default"
+# DOCKER_DIR="Get from commit dir"
