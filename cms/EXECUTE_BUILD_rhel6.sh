a4436f3c9cf6ed21fb09c74f94ba5d7ea998ccc2
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2018-12-14 10:21:55 +0100

    EXTRA_BUILD_ARGS on single line

--- cms/EXECUTE_BUILD_rhel6.sh
+++ cms/EXECUTE_BUILD_rhel6.sh
@@ -1,8 +1,7 @@
 # Do not foget to change tag on each update or prevous version will be overwriten
 CONTAINER_TAG=rhel6
 # DOCKERHUB_USER="Use job default"
-EXTRA_BUILD_ARGS="--build-arg OSG_WN_TAG=3.4-el6"
-EXTRA_BUILD_ARGS="--build-arg BUILD_DATE=$(date +%Y%m%d-%H%m) ${EXTRA_BUILD_ARGS}"
+EXTRA_BUILD_ARGS="--build-arg OSG_WN_TAG=3.4-el6 --build-arg BUILD_DATE=$(date +%Y%m%d-%H%m)"
 #PUSH_CONTAINER=true
 # DOCKER_FILE=Dockerfile
 #CONTAINER_NAME=cms
