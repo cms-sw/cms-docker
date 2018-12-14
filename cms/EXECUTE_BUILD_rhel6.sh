5b210686fa7d4c8d887df2881735a2733b0d207d
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2018-12-14 11:46:57 +0100

    added missing packages

--- cms/EXECUTE_BUILD_rhel6.sh
+++ cms/EXECUTE_BUILD_rhel6.sh
@@ -1,7 +1,8 @@
+# - 14 Dec 2018: compat-readline5 needed by slc6_amd64_gcc462 bootstrap
 # Do not foget to change tag on each update or prevous version will be overwriten
 CONTAINER_TAG=rhel6
 # DOCKERHUB_USER="Use job default"
-EXTRA_BUILD_ARGS="--build-arg OSG_WN_TAG=3.4-el6 --build-arg BUILD_DATE=$(date +%Y%m%d-%H%m)"
+EXTRA_BUILD_ARGS="--build-arg OSG_WN_TAG=3.4-el6 --build-arg BUILD_DATE=$(date +%Y%m%d-%H%m) --build-arg EXTRA_PACKAGES=compat-readline5"
 #PUSH_CONTAINER=true
 # DOCKER_FILE=Dockerfile
 #CONTAINER_NAME=cms
--- cms/EXECUTE_BUILD_rhel7.sh
+++ cms/EXECUTE_BUILD_rhel7.sh
@@ -1,7 +1,8 @@
+# - 14 Dec 2018: bzip2 needed by slc7_amd64_gcc530 bootstrap
 # Do not foget to change tag on each update or prevous version will be overwriten
 CONTAINER_TAG=rhel7
 # DOCKERHUB_USER="Use job default"
-EXTRA_BUILD_ARGS="--build-arg OSG_WN_TAG=3.4-el7 --build-arg BUILD_DATE=$(date +%Y%m%d-%H%m)"
+EXTRA_BUILD_ARGS="--build-arg OSG_WN_TAG=3.4-el7 --build-arg BUILD_DATE=$(date +%Y%m%d-%H%m) --build-arg EXTRA_PACKAGES=bzip2"
 #PUSH_CONTAINER=true
 # DOCKER_FILE=Dockerfile
 #CONTAINER_NAME=cms
