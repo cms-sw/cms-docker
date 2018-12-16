50d0564312553ec7643c19343f5f1e6b3f74db90
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2018-12-16 12:24:59 +0100

    added test and auto build for slc7-instaler

---
+++ slc7-installer/EXECUTE_BUILD_MAIN.sh
@@ -0,0 +1,9 @@
+# Do not foget to change tag on each update or prevous version will be overwriten
+# CONTAINER_TAG=latest
+# DOCKERHUB_USER="Use job default"
+# EXTRA_BUILD_ARGS=
+PUSH_CONTAINER=true
+# DOCKER_FILE=Dockerfile
+#CONTAINER_NAME=cc7
+# GITHUT_REPO="Use job default"
+# DOCKER_DIR="Get from commit dir"
