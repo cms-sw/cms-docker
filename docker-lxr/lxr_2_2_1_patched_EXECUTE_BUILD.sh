b9ec51228ee62ef22a1482907a858d1eefbd3d86
Zygimantas Matonis <zygimantas.matonis@cern.ch>
2019-01-30 14:45:01 +0100

    Build docekr-lxr container automaticly

---
+++ docker-lxr/lxr_2_2_1_patched_EXECUTE_BUILD.sh
@@ -0,0 +1,9 @@
+# Do not foget to change tag on each update or prevous version will be overwriten
+CONTAINER_TAG=2.2.1
+#DOCKERHUB_USER="Use job default"
+#EXTRA_BUILD_ARGS=
+PUSH_CONTAINER=true
+# DOCKER_FILE="Use job default"
+# CONTAINER_NAME="Get from commit dir"
+# GITHUT_REPO="Use job default"
+# DOCKER_DIR="Get from commit dir"
