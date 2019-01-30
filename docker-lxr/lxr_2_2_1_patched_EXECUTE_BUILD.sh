f67bfef293bc96211741540ab58edab3ad5da207
Zygimantas Matonis <zygimantas.matonis@cern.ch>
2019-01-30 15:27:07 +0100

    Update lxr_2_2_1_patched_EXECUTE_BUILD.sh
    lxr tag changed to latest

--- docker-lxr/lxr_2_2_1_patched_EXECUTE_BUILD.sh
+++ docker-lxr/lxr_2_2_1_patched_EXECUTE_BUILD.sh
@@ -1,5 +1,5 @@
 # Do not foget to change tag on each update or prevous version will be overwriten
-CONTAINER_TAG=2.2.1
+# CONTAINER_TAG#2.2.1
 #DOCKERHUB_USER="Use job default"
 #EXTRA_BUILD_ARGS=
 PUSH_CONTAINER=true
