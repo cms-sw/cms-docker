c94bf4768aac778963bea796a01cf8f4a9d3fda3
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2019-04-10 15:46:34 +0200

    added ppc64le buildfile

---
+++ cc7/EXECUTE_BUILD_PPC64LE.sh
@@ -0,0 +1,9 @@
+# Do not foget to change tag on each update or prevous version will be overwriten
+# CONTAINER_TAG=latest
+# DOCKERHUB_USER="Use job default"
+# EXTRA_BUILD_ARGS=
+PUSH_CONTAINER=true
+DOCKER_FILE=Dockerfile.ppc64le  
+CONTAINER_NAME=cc7-ppc64le
+# GITHUT_REPO="Use job default"
+# DOCKER_DIR="Get from commit dir"
