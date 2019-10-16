7297cc76ea03d6199c6283133f538ef3a0b89709
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2019-10-16 13:07:11 +0200

    addd docker_unpack; cleanup

--- bin/docker_utils.py
+++ bin/docker_utils.py
@@ -9,10 +9,8 @@ from get_image_config import get_docker_images
 import json
 
 if sys.version_info[0] == 2:
-# python 2 module
   from commands import getstatusoutput as run_cmd
 else:
-# python 3 module
   from subprocess import getstatusoutput as run_cmd
 
 def get_docker_token(repo):
@@ -20,22 +18,20 @@ def get_docker_token(repo):
   e, o = run_cmd('curl --silent --request "GET" "https://auth.docker.io/token?service=registry.docker.io&scope=repository:%s:pull"' % repo)
   return loads(o)['token']
 
-def get_docker_image_manifest(repo, tag):
+def get_manifest(image):
+  repo = image.split(":",1)[0]
+  tag = image.split(":",1)[-1]
+  if repo==tag: tag="latest"
   token = get_docker_token(repo)
   print('Getting image_manifest for %s/%s' % (repo, tag))
   e, o = run_cmd('curl --silent --request "GET" --header "Authorization: Bearer %s" "https://registry-1.docker.io/v2/%s/manifests/%s"' % (token, repo, tag))
-  image_manifest=loads(o)
-  fsLayers=image_manifest['fsLayers']
-  list_of_digests = []
-  print('echo fsLayers of %s:%s:' % (repo, tag))
-  for a in fsLayers:
-    print(a)
-  return fsLayers
+  return loads(o)
 
-def has_parent_changed(parent_image, inherited_image, repository):
-  parent_image_list = get_docker_image_manifest(*parent_image.split(':'))
-  inherited_image_list = get_docker_image_manifest(*inherited_image.split(':'))
-  while parent_image_list:
-    if inherited_image_list.pop() != parent_image_list.pop():
+def has_parent_changed(parent, image, repository):
+  parent_layers = get_manifest(parent)['fsLayers']
+  image_layers = get_manifest(image)['fsLayers']
+  while parent_layers and image_layers:
+    if parent_layers.pop()!=image_layers.pop():
       return True
-  return False
+  return len(parent_layers)>0
+
