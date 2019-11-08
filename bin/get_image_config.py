9b11c6e484fc04216c3914c3b870c6bd60b4c2a1
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2019-11-08 14:16:05 +0100

    added cmssw/cms monthly image tags

--- bin/get_image_config.py
+++ bin/get_image_config.py
@@ -4,13 +4,15 @@ import yaml
 import sys, re
 from os.path import exists, join, dirname, abspath
 from docker_utils import get_manifest
+from datetime import datetime
+now = datetime.now()
 
-regex_var = re.compile('^(.*)\$\{([^}]+)\}(.*)$')
+regex_var = re.compile('^(.*?)([$]{1,2})\{([^}]+)\}(.*)$')
 
 def push_info(setup, data, variables=False):
   data.append({})
   for k in setup:
-    if k == 'tags': continue
+    if k in ['tags', 'groups']: continue
     if type(setup[k])==dict:
       push_info(setup[k], data, variables or k=="variables")
     else:
@@ -30,6 +32,25 @@ def get_key(key, data):
     if key in item: return item[key]
   return ""
 
+def expand_var(var, data):
+  stack = []
+  expanded = {}
+  while True:
+    stack.append(var)
+    m = regex_var.match(var)
+    if not m: return var
+    vx = m.group(3)
+    if vx in expanded:
+      print("Error: variables %s refers to itself" %  vx)
+      print("  ","\n  ".join(stack))
+      sys.exit(1)
+    expanded[vx] = 1
+    if m.group(2)=='$$': vx = eval(m.group(3))
+    else: vx = get_key(m.group(3), data)
+    vx = vx.replace("%s{%s}" % (m.group(2),m.group(3)), '')
+    var = "%s%s%s" % (m.group(1), vx, m.group(4))
+  return v
+
 def expand(data):
   nbuilds = []
   for item in data:
@@ -37,34 +58,22 @@ def expand(data):
     for k in item:
       if k=='.variables': continue
       v = item[k]
-      while True:
-        m = regex_var.match(v)
-        if not m: break
-        v = "%s%s%s" % (m.group(1), get_key(m.group(2), data), m.group(3))
-      nbuilds[-1][k]=v
+      k = expand_var(k, data)
+      nbuilds[-1][k] = expand_var(v, data)
   return nbuilds
 
-def get_docker_images(name, repository='cmssw'):
-  images = []
-  setup_file = join(dirname(dirname(abspath(__file__))), name, "config.yaml")
-  if not exists(setup_file):
-    print("Warnings: No such file %s" % setup_file)
-    return images
-  setup = yaml.load(open(setup_file))
-  data = [{}]
-  data[-1]['repository'] = repository
-  data[-1]['name'] = name
-  data[-1]['contianer'] = join(repository, name)
-  push_info(setup, data)
+def process_tags(setup, data, images):
+  if 'tags' not in setup: return
   for tag in setup['tags']:
     cnt = len(data)
-    push_info(setup['tags'][tag], data)
+    if setup['tags'][tag]:
+      push_info(setup['tags'][tag], data)
     data[-1]['tag']=tag
     img_data = expand(data)
     pop_info(data, cnt)
     image_name = get_key('contianer', img_data) + ":"+get_key('tag', img_data)
-    override = get_key('override', img_data)
-    if override in ["", "False"]:
+    override = get_key('override', img_data).upper()
+    if override in ["", "FALSE"]:
       manifest = get_manifest(image_name)
       if 'fsLayers' in manifest: continue
       if not 'errors' in manifest: continue
@@ -94,6 +103,34 @@ def get_docker_images(name, repository='cmssw'):
     if ".variables" in data[0]:
       for v in data[0][".variables"]:
         images[-1][v] = get_key(v, img_data)
+  return
+  
+
+def process_groups(setup, data, images):
+  if 'groups' not in setup: return
+  for group in setup['groups']:
+    cnt = len(data)
+    push_info(setup['groups'][group], data)
+    process_tags(setup['groups'][group], data, images)
+    process_groups(setup['groups'][group], data, images)
+    pop_info(data, cnt)
+
+def get_docker_images(name, repository='cmssw'):
+  images = []
+  setup_file = join(dirname(dirname(abspath(__file__))), name, "config.yaml")
+  if not exists(setup_file):
+    print("Warnings: No such file %s" % setup_file)
+    return images
+  setup = yaml.load(open(setup_file))
+  data = [{}]
+  data[-1]['repository'] = repository
+  data[-1]['name'] = name
+  data[-1]['contianer'] = join(repository, name)
+  push_info(setup, data)
+  if not 'groups' in setup:
+    setup['groups'] = {'default' : {'tags': dict(setup['tags'])}}
+    del setup['tags']
+  process_groups(setup, data, images)
   return images
 
 if __name__ == "__main__":
