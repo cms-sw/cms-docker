fd1e05841634b6e7b416a467415a0199f0d03046
Roman <romano@linuxmail.org>
2019-11-21 15:50:15 +0100

    Imports Loader if yaml version has it

--- bin/get_image_config.py
+++ bin/get_image_config.py
@@ -127,9 +127,11 @@ def get_docker_images(name, repository='cmssw'):
     print("Warnings: No such file %s" % setup_file)
     return images
   with open(setup_file) as file:
-    if sys.version_info[0] > 2:
+    try:
+      from yaml import Loader
       setup = yaml.load(file, Loader=yaml.FullLoader)
-    else:
+    except ImportError as e:
+      print('%s. Update yaml module' % e)
       setup = yaml.load(file)
   data = [{}]
   data[-1]['repository'] = repository
