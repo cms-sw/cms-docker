5595d331ddeebf7c390762357a97fa45277afd5e
Roman <romano@linuxmail.org>
2019-12-05 17:50:03 +0100

    new docker tools added to docker_utils

--- bin/docker_utils.py
+++ bin/docker_utils.py
@@ -8,56 +8,154 @@ from os.path import expanduser
 from requests.exceptions import HTTPError
 import os, glob, sys
 
-DOCKER_REGISTRY_API='https://registry-1.docker.io/v2/'
-DOCKER_HUB_API='https://hub.docker.com/v2/'
+DOCKER_REGISTRY_API='https://registry-1.docker.io/v2'
+DOCKER_HUB_API='https://hub.docker.com/v2'
 
-def http_request(url, data=None, headers=None, method = 'GET', json=True):
-  if data is None: data = {}
-  if headers is None: headers = {}
-  try:
-    response = request(method=method, url=url, data=data, headers=headers)
-    if response.status_code == 204:
-      return {} if json else '{}'
-    return loads(response.content) if json else response
-  except HTTPError as error:
-    if error.code == 401:
-      return {} if json else '{}'
-  return {} if json else '{}'
+def http_request(url, data=None, headers=None, method = 'GET', json=False):
+  response = request(method=method, url=url, data=data, headers=headers)
+  if not response.ok:
+    return response
+  return response.json() if json else response
 
 # get token for docker Registry API
 def get_docker_token(repo):
   return http_request('https://auth.docker.io/token?service=registry.docker.io\
-&scope=repository:%s:pull' % repo)['token']
+&scope=repository:%s:pull' % repo, json=True)['token']
 
 # get token for Docker HUB API:
-def get_dockerHubToken():
-  url = '%susers/login/' % DOCKER_HUB_API
-  docker_token = open(expanduser("~/.docker-token")).read().strip()
-  data = loads(docker_token)
-  response = http_request(url, data, None, method='POST')
+def get_dockerHubToken(DOCKER_HUB_API):
+  url = '%s/users/login/' % DOCKER_HUB_API
+  secret = open(expanduser("~/.docker-token")).read().strip()
+  response = http_request(url, loads(secret), method = 'POST', json=True)
   return response['token']
+
+def get_repos(DOCKER_HUB_API, user):
+  url = '%s/repositories/%s/' % (DOCKER_HUB_API, user)
+  response = http_request(url, json=True)
+  repos = []
+  try: response['results']
+  except TypeError:
+    return response, response.reason
+  for repo in response['results']:
+    repos.append(str(repo['name']))
+  return repos
+
+def create_repo(DOCKER_HUB_API, token, user, repo, private=False):
+  url = '%s/repositories/' % DOCKER_HUB_API
+  headers = {}
+  headers['Authorization'] = 'JWT %s' % token
+  payload = {
+    "namespace":"%s" % user,
+    "name":"%s" % repo,
+    "is_private":"%s" % private
+  }
+  response = http_request(url, payload, headers, method = 'POST')
+  return response.content
+
+def delete_repo(DOCKER_HUB_API, user, repo, token):
+  url = '%s/repositories/%s/%s' % (DOCKER_HUB_API, user, repo)
+  headers = {}
+  headers['Authorization'] = 'JWT %s' % token
+  response = http_request(url, headers=headers, method = 'DELETE')
+  return response, response.reason
+
+def get_members(DOCKER_HUB_API, orgname, teamname, token):
+  url = '%s/orgs/%s/groups/%s/members/' % (DOCKER_HUB_API, orgname, teamname)
+  headers = {}
+  headers['Authorization'] = 'JWT %s' % token
+  response = http_request(url, headers=headers, json=True)
+  members = []  
+  for member in response:
+    try: members.append(str(member['username']))
+    except TypeError:
+      return response
+  return members
+
+def add_member(DOCKER_HUB_API, orgname, teamname, token, member):
+  url = '%s/orgs/%s/groups/%s/members/' % (DOCKER_HUB_API, orgname, teamname)
+  headers = {}
+  headers['Authorization'] = 'JWT %s' % token
+  data = {"member":"%s" % member}
+  response = http_request(url, data=data, headers=headers, method = 'POST')
+  return response, response.reason
+
+def delete_member(DOCKER_HUB_API, orgname, teamname, member, token):
+  url = '%s/orgs/%s/groups/%s/members/%s/' % (DOCKER_HUB_API, orgname, teamname, member)
+  headers = {}
+  headers['Authorization'] = 'JWT %s' % token
+  response = http_request(url, headers=headers, method = 'DELETE')
+  return response, response.reason
+
+def get_teams(DOCKER_HUB_API, orgname, token):
+  url = '%s/orgs/%s/groups/' % (DOCKER_HUB_API, orgname)
+  headers = {}
+  headers['Authorization'] = 'JWT %s' % token
+  response = http_request(url, headers=headers)
+  teams = {}
+  try: response.json()['results']
+  except KeyError:
+    return response, response.content
+  for team in response.json()['results']:
+    key = str(team['name'])
+    value = str(team['id'])
+    teams[key] = value
+  return teams
+
+def create_team(DOCKER_HUB_API, orgname, token, teamname):
+  url = '%s/orgs/%s/groups/' % (DOCKER_HUB_API, orgname)
+  headers = {}
+  headers['Authorization'] = 'JWT %s' % token
+  data = {"name":"%s" % teamname}
+  response = http_request(url, data=data, headers=headers, method = 'POST')
+  return response, response.content
+
+def delete_team(DOCKER_HUB_API, orgname, teamname, token):
+  url = '%s/orgs/%s/groups/%s' % (DOCKER_HUB_API, orgname, teamname)
+  headers = {}
+  headers['Authorization'] = 'JWT %s' % token
+  response = http_request(url, headers=headers, method = 'DELETE')
+  return response, response.reason
+
+# permissions must be: 'read' / 'write' / 'admin'
+def add_permissions(DOCKER_HUB_API, orgname, repo, group_id, token, permission):
+  url = '%s/repositories/%s/%s/groups/' % (DOCKER_HUB_API, orgname, repo)
+  headers = {}
+  headers['Authorization'] = 'JWT %s' % token
+  payload = {"group_id" : "%s" % group_id, "permission" : "%s" % permission}
+  response = http_request(url, data=payload, headers=headers, method = 'POST')
+  return response, response.content
+
+def delete_permissions(DOCKER_HUB_API, orgname, repo, group_id, token):
+  url = '%s/repositories/%s/%s/groups/%s' % (DOCKER_HUB_API, orgname, repo, group_id)
+  headers = {}
+  headers['Authorization'] = 'JWT %s' % token
+  response = http_request(url, headers=headers, method = 'DELETE')
+  return response, response.reason
 
 def deleteTag(token, repo, tag, dryRun):
   print('** Deleting tag: %s from %s repository....'% (tag, repo))
   if dryRun: return {}
-  url = '%srepositories/%s/tags/%s/' % (DOCKER_HUB_API, repo, tag)
+  url = '%s/repositories/%s/tags/%s/' % (DOCKER_HUB_API, repo, tag)
   headers = {}
   headers['Accept'] = 'application/json'
   headers['Authorization'] = 'JWT %s' % token
   response = http_request(url, None, headers, method = 'DELETE')
-  return response
+  return response, response.reason
 
 def logout(token):
-  url = '%slogout/' % DOCKER_HUB_API
+  url = '%s/logout/' % DOCKER_HUB_API
   headers = {}
   headers['Accept'] = 'application/json'
   headers['Authorization'] = 'JWT %s' % token
   response = http_request(url, None, headers, method='POST')
-  print(response)
+  return response.content
 
 def get_digest_of_image(repo, tag):
   url = '%s/repositories/%s/tags/%s' % (DOCKER_HUB_API, repo, tag)
-  return http_request(url, json=True)['images'][0]['digest'].split(":")[-1]
+  try:
+    return http_request(url, json=True)['images'][0]['digest'].split(":")[-1]
+  except TypeError:
+    return http_request(url)
 
 def get_manifest(image):
   repo = image.split(":",1)[0]
@@ -65,20 +163,21 @@ def get_manifest(image):
     repo = 'library/'+repo
   tag = image.split(":",1)[-1]
   if repo==tag: tag="latest"
-  url = '%s%s/manifests/%s' % (DOCKER_REGISTRY_API, repo, tag)
+  url = '%s/%s/manifests/%s' % (DOCKER_REGISTRY_API, repo, tag)
   token = get_docker_token(repo)
   headers = {}
   headers['Authorization'] = 'Bearer %s' % token
-  response = http_request(url, None, headers)
+  response = http_request(url, None, headers, json=True)
   return response
 
 def get_tags(image):
   token = get_docker_token(image)
   headers = {}
   headers['Authorization'] = 'Bearer %s' % token
-  url = '%s%s/tags/list?n=500' % (DOCKER_REGISTRY_API, image)
-  response = http_request(url, None, headers)
-  return response['tags']
+  url = '%s/%s/tags/list?n=500' % (DOCKER_REGISTRY_API, image)
+  response = http_request(url, None, headers, json=True)
+  try: return response['tags']
+  except TypeError: return response
 
 def has_parent_changed(parent, image):
   image_manifest = get_manifest(image)
