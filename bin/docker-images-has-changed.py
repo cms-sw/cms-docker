784e0780ba3b66a2b1b162e9c5a5972794de2a64
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2020-01-06 17:35:59 +0100

    use previous dail/month tags

--- bin/docker-images-has-changed.py
+++ bin/docker-images-has-changed.py
@@ -34,7 +34,6 @@ autoTag = re.compile("^[^:]+:(.*-[dm]2)\d{5}$")
 for reponame in repos:
   oldtags = get_tags("cmssw/"+reponame)
   oldtags.sort(reverse=True)
-  print(oldtags)
   for img in get_docker_images(reponame):
     if tags and (not img['IMAGE_TAG'] in tags): continue
     buildimg = args.force
@@ -44,7 +43,6 @@ for reponame in repos:
       m = autoTag.match(inher)
       if m:
         tag_base = m.group(1)
-        print(tag_base)
         for tag in oldtags:
           if tag.startswith(m.group(1)):
             inher = "cmssw/"+reponame+":"+tag
