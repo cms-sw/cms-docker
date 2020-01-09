aca612821ad4b15550205b4e5c0e399ff65a01e4
Roman <romano@linuxmail.org>
2020-01-09 15:58:18 +0100

    docker_sync script and docker utilstested

--- bin/docker_sync.py
+++ bin/docker_sync.py
@@ -9,7 +9,7 @@ import sys
 from os.path import join, dirname, abspath
 
 parser = ArgumentParser(description='Synchronize Docker HUB with yaml configuration file')
-parser.add_argument('-u', '--username', dest='username', help="Provide Docker Hub username for synchronization", type=str, default='org0namespace')
+parser.add_argument('-u', '--username', dest='username', help="Provide Docker Hub username for synchronization", type=str, default='cmssw')
 parser.add_argument('-n', '--disable', dest='dryrun', help="Dry Run mode enabled by default. Disable it to make changes to docker hub", action="store_false", default=True)
 args = parser.parse_args()
 if not args.dryrun:
@@ -53,8 +53,6 @@ def update_dockerhub(config_file, docker_hub, username = args.username, team_nam
 # UPDATE REPOSITORIES:
 print('\n----- Synchronizing repositories for "%s":' % args.username)
 hub_repos = get_repos(args.username)
-print(hub_repos)
-print(yaml_file['repositories'])
 if not hub_repos[0]: print(hub_repos[1]) & sys.exit(1)
 if hub_repos[1] == []:
   print('No repositories found. Check Docker Hub username')
@@ -63,8 +61,6 @@ update_dockerhub(yaml_file['repositories'], hub_repos[1], what_to_sync='repos')
 # UPDATE TEAMS:
 print('\n----- Synchronizing teams for "%s":' % args.username)
 hub_teams = get_teams(args.username)
-print(hub_teams)
-print(yaml_file['teams'].keys())
 if not hub_teams[0]: print(hub_teams[1]) & sys.exit(1)
 update_dockerhub(list(yaml_file['teams'].keys()), list(hub_teams[1].keys()), what_to_sync='teams')
 for team_name in hub_teams[1]:
@@ -73,26 +69,30 @@ for team_name in hub_teams[1]:
   # UPDATE PERMISSIONS:
     print('\n----- Synchronizing permissions for "%s" team:' % team_name)
     hub_permissions = get_permissions(args.username, team_name)
-    print(hub_permissions)
     if not hub_permissions[0]: print(hub_permissions[1]) & sys.exit(1)
     yaml_permissions = yaml_file['teams'][team_name]['permissions']
-    print(yaml_permissions)
     if yaml_permissions is None:
       print('No repository permissions found in yaml config file for "%s"' % team_name)
-      yaml_repo_access = []
+      yaml_access_list = []
     else:
-      yaml_repo_access = yaml_permissions.keys()
-    docker_repo_access = []
-    docker_repo_access = hub_permissions[1].keys()
-    update_dockerhub(yaml_repo_access, docker_repo_access, team_name=team_name, 
+      yaml_access_list = yaml_permissions.keys()
+    permissions_to_add = {}
+    for item in hub_permissions[1].items():
+      repository, permission = item[0], item[1]
+      if yaml_permissions and yaml_permissions[repository] != permission:
+        continue
+      permissions_to_add[repository] = permission
+    docker_access_list = []
+    docker_access_list = permissions_to_add.keys()
+    update_dockerhub(yaml_access_list, docker_access_list, team_name=team_name, 
     team_id=team_id, yaml_permissions=yaml_permissions, what_to_sync = 'permissions')
     # UPDATE MEMBERS:
     print('\n----- Synchronizing members for "%s":' % team_name)
     hub_team_members = get_members(args.username, team_name)
-    print(hub_team_members)
     if not hub_team_members[0]: print(hub_team_members[1]) & sys.exit(1)
     members_in_yaml = yaml_file['teams'][team_name]['members']
-    print(members_in_yaml)
-    if members_in_yaml is None: members_in_yaml = []
+    if members_in_yaml is None: 
+      print('No members found in yaml config file for "%s"' % team_name)
+      members_in_yaml = []
     update_dockerhub(members_in_yaml, hub_team_members[1], team_name=team_name, what_to_sync='members')
 logout()
