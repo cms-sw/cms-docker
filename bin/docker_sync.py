c18b16a5554b9d192ac76047fd8903dde80003b7
Roman <romano@linuxmail.org>
2020-01-09 23:58:19 +0100

    docker-sync ready to use with updated yaml config

--- bin/docker_sync.py
+++ bin/docker_sync.py
@@ -14,9 +14,6 @@ parser.add_argument('-n', '--disable', dest='dryrun', help="Dry Run mode enabled
 args = parser.parse_args()
 if not args.dryrun:
   print('==== DRY RUN MODE DISABLED ====')
-yaml_location = join(dirname(dirname(abspath(__file__))), "docker_config.yaml")
-with open(yaml_location) as file:
-  yaml_file = yaml.load(file, Loader=yaml.FullLoader)
 
 def update_dockerhub(config_file, docker_hub, username = args.username, team_name = None, repo = None, 
                      team_id = None, yaml_permissions = None, what_to_sync = None, dryrun=args.dryrun):
@@ -39,7 +36,7 @@ def update_dockerhub(config_file, docker_hub, username = args.username, team_nam
     if list_item in docker_hub and list_item not in config_file:
       if what_to_sync == 'repos':
         print('Deleting repository: "%s"' % list_item)
-        print(dryrun_message) if dryrun else print(delete_repo(username, list_item)[0])
+        print(dryrun_message) if dryrun else print('%s repository does not exist in yaml configuration file' % list_item) & sys.exit(1)
       elif what_to_sync == 'teams':
         print('Deleting team: "%s"' % list_item)
         print(dryrun_message) if dryrun else print(delete_team(username, list_item)[0])
@@ -50,6 +47,14 @@ def update_dockerhub(config_file, docker_hub, username = args.username, team_nam
         print('Deleting member "%s" from "%s" team:' % (list_item, team_name))
         print(dryrun_message) if dryrun else print(delete_member(username, team_name, list_item)[0])
 
+yaml_location = join(dirname(dirname(abspath(__file__))), "docker_config.yaml")
+with open(yaml_location) as file:
+  yaml_file = yaml.load(file, Loader=yaml.FullLoader)
+repos_config_dict = yaml_file['repositories']
+yaml_repos = []
+for signle_repo_config in repos_config_dict:
+  yaml_repos += (signle_repo_config.keys())
+yaml_teams = yaml_file['teams'].keys()
 # UPDATE REPOSITORIES:
 print('\n----- Synchronizing repositories for "%s":' % args.username)
 hub_repos = get_repos(args.username)
@@ -57,21 +62,28 @@ if not hub_repos[0]: print(hub_repos[1]) & sys.exit(1)
 if hub_repos[1] == []:
   print('No repositories found. Check Docker Hub username')
   sys.exit(1)
-update_dockerhub(yaml_file['repositories'], hub_repos[1], what_to_sync='repos')
+update_dockerhub(yaml_repos, hub_repos[1], what_to_sync='repos')
 # UPDATE TEAMS:
 print('\n----- Synchronizing teams for "%s":' % args.username)
 hub_teams = get_teams(args.username)
 if not hub_teams[0]: print(hub_teams[1]) & sys.exit(1)
-update_dockerhub(list(yaml_file['teams'].keys()), list(hub_teams[1].keys()), what_to_sync='teams')
+update_dockerhub(yaml_teams, list(hub_teams[1].keys()), what_to_sync='teams') 
+team_acces_map = {}
 for team_name in hub_teams[1]:
   team_id = hub_teams[1][team_name]
+  yaml_permissions = {}
   if team_name != 'owners':
   # UPDATE PERMISSIONS:
     print('\n----- Synchronizing permissions for "%s" team:' % team_name)
     hub_permissions = get_permissions(args.username, team_name)
     if not hub_permissions[0]: print(hub_permissions[1]) & sys.exit(1)
-    yaml_permissions = yaml_file['teams'][team_name]['permissions']
-    if yaml_permissions is None:
+    for signle_repo_config in repos_config_dict:
+      team_acces_map = signle_repo_config.values()[0]
+      try:
+        yaml_permissions[signle_repo_config.keys()[0]] = team_acces_map[team_name]
+      except Exception:
+        continue
+    if not yaml_permissions:
       print('No repository permissions found in yaml config file for "%s"' % team_name)
       yaml_access_list = []
     else:
@@ -90,7 +102,7 @@ for team_name in hub_teams[1]:
     print('\n----- Synchronizing members for "%s":' % team_name)
     hub_team_members = get_members(args.username, team_name)
     if not hub_team_members[0]: print(hub_team_members[1]) & sys.exit(1)
-    members_in_yaml = yaml_file['teams'][team_name]['members']
+    members_in_yaml = yaml_file['teams'][team_name]
     if members_in_yaml is None: 
       print('No members found in yaml config file for "%s"' % team_name)
       members_in_yaml = []
