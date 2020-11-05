2b2636a9b2b3c7703238801a2a395bfb57e59a52
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2020-11-05 16:29:15 +0100

    updated slc6 docker; added xrootd-client

--- slc6/EXECUTE_BUILD_MAIN.sh
+++
@@ -1,9 +0,0 @@
-# Do not foget to change tag on each update or prevous version will be overwriten
-# CONTAINER_TAG=latest
-# DOCKERHUB_USER="Use job default"
-# EXTRA_BUILD_ARGS=
-PUSH_CONTAINER=true
-# DOCKER_FILE=Dockerfile
-#CONTAINER_NAME=slc6
-# GITHUT_REPO="Use job default"
-# DOCKER_DIR="Get from commit dir"
