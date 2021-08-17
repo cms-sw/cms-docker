f0c3caeb99fe954e0cc3e17d6374cfea129051fc
saqibhaleem <saqib.haleem@gmail.com>
2021-08-17 12:50:41 +0200

    Update osg-wn-client-setup script

--- cms/osg-wn-client-setup.sh
+++ cms/osg-wn-client-setup.sh
@@ -1,12 +1,10 @@
-#!/bin/sh
-# Requested by Marco Mascheroni: To setuposg-wn-client env
-# 29.6.2021
+#!/bin/bash
+# Requested by Marco Mascheroni: To setup osg-wn-client env
+# 17.8.2021
 export CMS_SINGULARITYD_ENV="none"
-if [[ "$SINGULARITY_CONTAINER" =~ ^/cvmfs/singularity.opensciencegrid.org/cmssw/cms:rhel6(-itb)?$ ]] && [ -e "/cvmfs/oasis.opensciencegrid.org/osg-software/osg-wn-client/3.4/current/el6-x86_64/setup.sh" ]; then
-    export CMS_SINGULARITYD_ENV="3.4/el6-x86_64"
-    source /cvmfs/oasis.opensciencegrid.org/osg-software/osg-wn-client/3.4/current/el6-x86_64/setup.sh
-elif [[ "$SINGULARITY_CONTAINER" =~ ^/cvmfs/singularity.opensciencegrid.org/cmssw/cms:rhel7(-itb)?$ ]] && [ -e "/cvmfs/oasis.opensciencegrid.org/osg-software/osg-wn-client/3.4/current/el7-x86_64/setup.sh" ]; then
-    export CMS_SINGULARITYD_ENV="3.4/el7-x86_64"
-    source /cvmfs/oasis.opensciencegrid.org/osg-software/osg-wn-client/3.4/current/el7-x86_64/setup.sh
+if [[ -f "/image-source-info.txt" ]]; then
+    OSG_VERSION=$(cat /image-source-info.txt | cut -d: -f3 | cut -d- -f1)
+    OS_VERSION=$(cat /image-source-info.txt | cut -d: -f3 | cut -d- -f2)
+    export CMS_SINGULARITYD_ENV="$OSG_VERSION/$OS_VERSION-x86_64"
+    source /cvmfs/oasis.opensciencegrid.org/osg-software/osg-wn-client/$OSG_VERSION/current/$OS_VERSION-x86_64/setup.sh
 fi
-
