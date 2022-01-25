444b562b1e877757be189765676c7b7ca564b5c4
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2022-01-25 10:33:15 +0100

    added alma8 container

---
+++ alma8/fix_ssh_config.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+function fix_ssh_config()
+{
+  cat "$1" | while read line ; do
+    if [ $(echo "$line" | grep '^\s*Include\s\s*' | wc -l) -eq 0 ] ; then
+      echo "$line"
+    else
+      echo "#${line}"
+      cfgs=$(echo "$line" | sed 's|^\s*Include\s\s*||')
+      for cfg in $(ls $cfgs 2>/dev/null) ; do
+        echo "###  Start of $cfg  ###"
+        fix_ssh_config "$cfg"
+         echo "###  End of $cfg  ###"
+      done
+     fi
+  done
+}
+
+fix_ssh_config $1 > $1.tmp
+mv $1.tmp $1
