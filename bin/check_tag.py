f50d4c2a24e98acbfb0e02898a79466779c8a27a
Shahzad Malik Muzaffar <shahzad.malik.muzaffar@cern.ch>
2022-09-12 16:12:39 +0200

    use docekr registroy instead of /index.docker.io

--- bin/check_tag.py
+++ bin/check_tag.py
@@ -1,16 +1,9 @@
 #!/usr/bin/env python3
-import json, sys
-from subprocess import getstatusoutput as run_cmd
+import sys
+from docker_utils import get_tags
 
-e, o = run_cmd('curl --silent -f -lSL https://index.docker.io/v1/repositories/%s/tags' % sys.argv[1])
-if e:
-  print(o)
+res, data=get_tags(sys.argv[1])
+if res and (sys.argv[2] in data):
+  print("FOUND:%s" % sys.argv[2])
+else:
   sys.exit(1)
-
-data=json.loads(o)
-for container in data:
-  if container['name']==sys.argv[2]:
-    print("FOUND:%s" % sys.argv[2])
-    break
-
-sys.exit(0)
